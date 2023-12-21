79a3f2acd27184fb6b9806a38ee11b177ee5ab30
8328616507 <154434414+8328616507@users.noreply.github.com>
2023-12-21 17:29:37 +0530

    Add files via upload

---
+++ sample2.py
@@ -0,0 +1,211 @@
+#    sreedhar
+#   21/12/2023
+#   topic:operator
+
+i_num1 = 70
+i_num2 = 40
+i_num3 = 50
+result = i_num1+i_num2
+print("the sum of two integer:",id(i_num1),"enter the num2:",id(i_num2),)
+print(result)
+
+
+result = i_num1+i_num2
+print("the addition of two integer:", id(i_num1),"enter the num2:", id(i_num2),)
+print(result)
+
+
+result = i_num3-i_num2
+print("the sub of two integer:", id(i_num1),"enter the num2:", id(i_num2),)
+print(result)
+
+
+result = i_num1*i_num2
+print("the multiplication of two integer:", id(i_num1),"enter the num2:", id(i_num2),)
+print(result)
+
+result = i_num1 | i_num2
+print("the division of two integer:", id(i_num1),"enter the num2:", id(i_num2),)
+print(result)
+
+result = i_num1//i_num2
+print("the floor of two integer:", id(i_num1), "enter the num2:", id(i_num2),)
+print(result)
+
+result = i_num1 % i_num2
+print("the mod of two integer:", id(i_num1),"enter the num2:", id(i_num2),)
+print(result)
+
+result = i_num1**i_num2
+print("the power of two integer:", id(i_num1), "enter the num2:", id(i_num2),)
+print(result)
+
+
+num1 = 70.0
+num2 = 40.0
+num3 = 50.0
+result = num1+num2
+print("the sum of two integer:", id(num1), "enter the num2:", id(num2),)
+print(result)
+
+
+result = num1+num2
+print("the addition of two integer:", id(num1), "enter the num2:", id(num2),)
+print(result)
+
+
+result = num3-num2
+print("the sub of two integer:", id(num1), "enter the num2:", id(num2),)
+print(result)
+
+
+result = num1*num2
+print("the multiplication of two integer:", id(num1), "enter the num2:", id(num2),)
+print(result)
+#
+# result=num3|num2
+# print("the division of two integer:",id(num1),"enter the num2:",id(num2),)
+# print(result)
+
+result = num1//num2
+print("the floor of two integer:", id(num1), "enter the num2:", id(num2),)
+print(result)
+
+result = num1%num2
+print("the mod of two integer:", id(num1), "enter the num2:", id(num2),)
+print(result)
+
+result = i_num1**i_num2
+print("the power of two integer:", id(num1), " enter the num2:", id(num2),)
+print(result)
+
+
+num1 = 70.0j
+num2 = 40.0j
+num3 = 50.0j
+result = num1+num2
+print("the sum of two integer:", id(num1), "enter the num2:", id(num2),)
+print(result)
+
+
+result = num1+num2
+print("the addition of two integer:", id(num1), "enter the num2:", id(num2),)
+print(result)
+
+
+result = num3-num2
+print("the sub of two integer:", id(num1), "enter the num2:", id(num2),)
+print(result)
+
+
+result = num1*num2
+print("the multiplication of two integer:", id(num1), "enter the num2:", id(num2),)
+print(result)
+#
+# result=num1|num2
+# print("the division of two integer:",id(num1),"enter the num2:",id(num2),)
+# print(result)
+
+# result=num1//num2
+# print("the floor of two integer:",id(num1),"enter the num2:",id(num2),)
+# print(result)
+
+# result=num1 % num2
+# print("the mod of two integer:",id(num1),"enter the num2:",id(num2),)
+# print(result)
+
+result = i_num1**i_num2
+print("the power of two integer:", id(num1),"enter the num2:", id(num2),)
+print(result)
+
+
+#topic : Assignment Operators
+
+num1 = 70
+num1 += 30
+print('enter the number:', num1, id(num1))
+
+num1 = 70
+num1 -= 30
+print('enter the number:', num1, id(num1))
+
+num1 = 70
+num1 *= 30
+print('enter the number:', num1, id(num1))
+
+num1 = 70
+num1 |= 30
+print('enter the number:', num1, id(num1))
+
+num1 = 70
+num1 %= 30
+print('enter the number:', num1, id(num1))
+
+num1 = 70
+num1 //= 30
+print('enter the number:', num1, id(num1))
+
+num1 = 70
+num1 **= 30
+print('enter the number:', num1, id(num1))
+
+#float
+
+num1 = 25.6
+num1 += 50
+print('enter the number:', num1, id(num1))
+
+num1 = 25.6
+num1 -= 50
+print('enter the number:', num1, id(num1))
+
+num1 = 25.6
+num1 *= 50
+print('enter the number:', num1, id(num1))
+
+num1 = 25.6
+num1 |= 50
+print('enter the number:', num1, id(num1))
+
+num1 = 25.6
+num1 //= 50
+print('enter the number:', num1, id(num1))
+
+num1 = 25.6j
+num1 %= 50
+print('enter the number:', num1, id(num1))
+
+num1 = 25.6
+num1 **= 50
+print('enter the number:', num1, id(num1))
+
+# complex
+
+num1 = 25.6j
+num1 += 50
+print('enter the number:', num1, id(num1))
+
+num1 = 25.6j
+num1 -= 50
+print('enter the number:', num1, id(num1))
+
+num1 = 25.6j
+num1 *= 50
+print('enter the number:', num1, id(num1))
+
+num1 = 25.6j
+num1 |= 50
+print('enter the number:', num1, id(num1))
+
+num1 = 25.6j
+num1 //= 50
+print('enter the number:', num1, id(num1))
+
+num1 = 25.6j
+num1 **= 50
+print('enter the number:', num1, id(num1))
+
+num1 = 25.6j
+num1 %= 50
+print('enter the number:', num1, id(num1))
+
