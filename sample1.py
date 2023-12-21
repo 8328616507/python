4846ae372cd5f5ebeebf3f8ba5de10b16354c7f9
8328616507 <154434414+8328616507@users.noreply.github.com>
2023-12-21 17:28:57 +0530

    Add files via upload

---
+++ sample1.py
@@ -0,0 +1,203 @@
+# sreedhar
+# 21-12-2023
+
+salary = 40000
+isCredited = False
+bSalary = bool(salary)
+print("the salary is credited;",isCredited,"check",bool(salary))
+updatedsalary = bSalary+1000
+print(updatedsalary)
+bupdatedsalary = bool(updatedsalary)
+print("the updatedsalary;",isCredited,"check",bool(bupdatedsalary),"the updatedsalary;",type(bupdatedsalary))
+print("it is downcasting")
+
+# int to float
+
+
+salary = 40000
+isCredited = False
+bSalary = float(salary)
+print("the salary is credited;",isCredited,"check",float(salary))
+updatedsalary = bSalary+1000
+print(updatedsalary)
+bupdatedsalary = float(updatedsalary)
+print("the updatedsalary;",isCredited,"check",float(bupdatedsalary),"the updatedsalary;",type(bupdatedsalary))
+print("it is uppercasting")
+
+# int to string
+
+salary = 40000
+bSalary = str(salary)
+print("the salary is credited;","check",str(salary),type(bSalary))
+updatedsalary = bSalary
+print(updatedsalary)
+aupdatedsalary = str(updatedsalary)
+print("the updatedsalary;",isCredited,"check",str(aupdatedsalary),"the updatedsalary;",type(aupdatedsalary))
+print("it is uppercasting")
+
+# int of complex
+
+salary = 40000.0j
+cSalary = complex(salary)
+print("the salary is credited;","check",complex(salary),type(cSalary))
+updatedsalary = cSalary
+print(updatedsalary)
+aupdatedsalary = complex(updatedsalary)
+print("the updatedsalary;",isCredited,"check",complex(aupdatedsalary),"the updatedsalary;",type(aupdatedsalary))
+print("it is uppercasting")
+
+# foat of boolean
+
+cost = 40000.04
+isCredited = False
+bcost = bool(cost)
+print("this is cost;",isCredited,"check",bool(cost))
+updatedcost = bcost+1000
+print(updatedcost)
+bupdatedcost = bool(updatedcost)
+print("the updatedcost;",isCredited,"check",bool(bupdatedcost),"the updatedcost;",type(bupdatedcost))
+print("it is downcasting")
+
+# float of int
+
+cost = 40000.05
+bcost = int(cost)
+print("this is cost;",isCredited,"check",int(cost))
+updatedcost = bcost+100
+print(updatedcost)
+bupdatedcost = int(updatedcost)
+print("the updatedcost;",isCredited,"check",int(bupdatedcost),"the updatedcost;",type(bupdatedcost))
+print("it is downcasting")
+
+# float of string
+
+cost = 40000.04
+bcost = str(cost)
+print("this is cost;",isCredited,"check",str(cost))
+updatedcost = bcost+"100"
+print(updatedcost)
+bupdatedcost = str(updatedcost)
+print("the updatedcost;",isCredited,"check",str(bupdatedcost),"the updatedcost;",type(bupdatedcost))
+print("it is upcasting")
+
+# float of complex
+
+cost = 40000.0
+bcost = complex(cost)
+print("this is cost;",isCredited,"check",complex(cost))
+updatedcost = bcost+100
+print(updatedcost)
+bupdatedcost = complex(updatedcost)
+print("the updatedcost;",isCredited,"check",complex(bupdatedcost),"the updatedcost;",type(bupdatedcost))
+print("it is upcasting")
+
+# str of boolean
+
+cost = "40000"
+isCredited = False
+bcost = bool(cost)
+print("this is cost;",isCredited,"check",bool(cost))
+updatedcost = bcost+100
+print(updatedcost)
+bupdatedcost = bool(updatedcost)
+print("the updatedcost;",isCredited,"check",bool(bupdatedcost),"the updatedcost;",type(bupdatedcost))
+print("it is downcasting")
+
+# str of  int
+
+cost = "40000"
+bcost = int(cost)
+print("this is cost;",isCredited,"check",int(cost))
+updatedcost = bcost+100
+print(updatedcost)
+bupdatedcost = int(updatedcost)
+print("the updatedcost;",isCredited,"check",int(bupdatedcost),"the updatedcost;",type(bupdatedcost))
+print("it is downcasting")
+
+# str of float
+
+cost = "40000"
+bcost = float(cost)
+print("this is cost;",isCredited,"check",float(cost))
+updatedcost = bcost+1000
+print(updatedcost)
+bupdatedcost = float(updatedcost)
+print("the updatedcost;",isCredited,"check",float(bupdatedcost),"the updatedcost;",type(bupdatedcost))
+print("it is down casting")
+
+# str of complex
+
+cost = "40000"
+bcost = complex(cost)
+print("this is cost;",isCredited,"check",complex(cost))
+updatedcost = bcost+100
+print(updatedcost)
+bupdatedcost = complex(updatedcost)
+print("the updatedcost;",isCredited,"check",complex(bupdatedcost),"the updatedcost;",type(bupdatedcost))
+print("it is upcasting")
+
+# complex of boolean
+
+cost = 40000.0j
+isCredited = False
+bcost = bool(cost)
+print("this is cost;",isCredited,"check",bool(cost))
+updatedcost = bcost+100
+print(updatedcost)
+bupdatedcost = bool(updatedcost)
+print("the upDatedCost;",isCredited,"check",bool(bupdatedcost),"the upDatedC ost;",type(bupdatedcost))
+
+# # complex of str
+
+cost = 40000.0j
+bcost = str(cost)
+print("this is cost;",isCredited,"check",str(cost))
+upDatedCost = bcost+"100"
+print(updatedcost)
+bupDatedCost = str(updatedcost)
+print("the upDatedCost;",isCredited,"check",str(bupdatedcost),"the upDatedCost;",type(bupdatedcost))
+# print("it is down casting")
+
+# boolean of int
+
+cost = True
+isCredited = False
+bcost = bool(cost)
+print("this is cost;",isCredited,"check",bool(cost))
+upDatedCost = bcost+100
+print(updatedcost)
+bupDatedCost = bool(updatedcost)
+print("the updatedcost;",isCredited,"check",bool(bupdatedcost),"the updatedcost;",type(bupdatedcost))
+
+# boolean of float
+
+cost = True
+bcost = float(cost)
+print("this is cost;",isCredited,"check",float(cost))
+upDatedCost = bcost+100
+print(updatedcost)
+bupDatedCost = float(updatedcost)
+print("the upDatedDost;",isCredited,"check",float(bupdatedcost),"the upDatedCost;",type(bupdatedcost))
+print("it is up casting")
+
+# boolean of str
+
+cost = True
+bcost = str(cost)
+print("this is cost;",isCredited,"check",str(cost))
+upDatedCost = bcost+"100"
+print(updatedcost)
+bupDatedCost = str(updatedcost)
+print("the upDatedDost;",isCredited,"check",str(bupdatedcost),"the upDatedCost;",type(bupdatedcost))
+print("it is upcasting")
+
+# boolean of complex
+
+cost = True
+bcost = complex(cost)
+print("this is cost;",isCredited,"check",complex(cost))
+upDatedCost = bcost+100
+print(updatedcost)
+bupDatedCost = complex(updatedcost)
+print("the upDatedCost;",isCredited,"check",complex(bupdatedcost),"the upDatedCost;",type(bupdatedcost))
+print("is upcasting")
