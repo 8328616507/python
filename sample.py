b1c9468b275335ffcc590294aa7c6f94b74f4cc8
8328616507 <154434414+8328616507@users.noreply.github.com>
2023-12-21 16:02:40 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,52 @@
+# sreedhar
+#date:20-12-2023
+#integer
+number=50
+print(number)
+print("type of number;",(type(number)),"enter address of number;",(id(number)))
+Number=number+10
+print(Number)
+print("type of update number;",(type(Number)),"enter address of number;",(id(Number)))
+#float
+number=20.78
+print(number)
+print("type of number;",(type(number)),"enter address of age;",(id(number)))
+Number=number+48.33
+print(Number)
+print("type of update number;",(type(Number)),"enter address of number;",(id(Number)))
+#complex
+number=45.68j
+print(number)
+print("type of number;",(type(number)),"enter address of number;",(id(number)))
+Number=number+48.33j
+print(Number)
+print("type of update age;",(type(Number)),"enter address of age;",(id(Number)))
+#string
+number="19"
+print(number)
+print("type of number;",(type(number)),"enter address of number;",(id(number)))
+Number=number+"48"
+print(Number)
+#boolean
+print("type of update number;",(type(Number)),"enter address of age;",(id(Number)))
+number= True
+print(number)
+print("type of number;",(type(number)),"enter address of number;",(id(number)))
+number= False
+print(number)
+print("type of updated number;",(type(number)),"enter address of number;",(id(number)))
+
+length=(int(input("enter the length of rectangle")))
+width=(float(input("enter the width of rectangle")))
+area=length*width
+print(area)
+
+base=(int(input("enter the base")))
+height=(float(input("enter the Height")))
+area=base*height/2
+print(area)
+
+radius=(int(input('enter the radius')))
+area=3.142*radius*radius
+print(area)
+
