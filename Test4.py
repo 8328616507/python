04978141402465a24edcaf877bc58e2818dc707f
8328616507 <154434414+8328616507@users.noreply.github.com>
2023-12-27 09:59:22 +0530

    Add files via upload

---
+++ Test4.py
@@ -0,0 +1,139 @@
+# sreedhar
+# 12/26/2023
+# strName = "sriedhar"
+# strRolNo = "STR005"
+# strAddress = '''road-5 phase-2
+#                 bc colony Kunool-500083'''
+# print(len(strName))
+# print(strName[3:5])
+# print(strName[-7:-4])
+# print(strName[::-5])
+
+
+# string method
+
+# string = "sreedharsrsdfhftktkloiptututrtyfgtjyugdh"
+# print(string.upper())
+#
+# string = "SYGDQKFQDJKDJWGHWEHRWEH"
+# print(string.lower())
+#
+# string = "srgygewqfuiurfffuiwqiuyiyfyriyr"
+# print(string.isupper())
+# string = "HHDKFHQHOITMKJEWHWONUG"
+# print(string.isupper())
+#
+#
+# string = "sreedhar"
+# print(string.islower())
+# string = "DYGJFTFRIBIYFFIHWKDJH"
+# print(string.islower())
+#
+# string = "sreedhar"
+# print(string.isalpha())
+# string = "sreedhar123"
+# print(string.isalpha())
+#
+# string = "123467889"
+# print(string.isalnum())
+# string = "1234werdfydtddd"
+# print(string.isalnum())
+# string = "1324243srdqgjhgqdjfgjdwf@"
+# print(string.isalnum())
+#
+#
+# string = "123456576686"
+# print(string.isnumeric())
+# string = "123456576686asee"
+# print(string.isnumeric())
+# string = "123456576686@"
+# print(string.isnumeric())
+#
+# # string = "Sreedhar "
+# # print(string.capitalize())
+#
+# string = "sryffuyoifiyyfyiyikqwqqkqyiytiyyyoitootouiwerihmasbwkewrkbwkrybkerbkjwbygbrytrtuhilu"
+# string = string.count('k')
+# print("the number of repeated character is k:", string)
+#
+# string = "sryffuyoifiyyfyiyikqwqqkqyiytiyyyotootouiwerihmasbwkewrkbwkrybkerbkjwbygbrytrtuhilu"
+# string = string.count('z')
+# print("the number of repeated character is z:", string)
+#
+# string = "skuredg"
+# string = string.index('u')
+# print("finding the index:", string)
+#
+# # string = "skuffgewkelkgjgkwreglgjlkkilkgkgjvjgkwgkhhghweggir"
+# # string = string.index('m')
+# # print("find the position:", string)
+#
+# string = "jbfuheiijfhwewbfkuhhfhfhhfioiofilirifknfjfijfjfkjfkjfif"
+# string = string.find('b')
+# print("finding the position:", string)
+#
+# string = "vgfwfgewfehfehwifliwnjfffewjfeyfgewfwfejkfhwejfekufkjfqwhf"
+# string = string.rfind('w')
+# print("find last character:", string)
+
+# strip
+# string = "  @#$%^&*!sreedhar!@#$%^&*   "
+# print("string:", string)
+# string = string.strip()
+# print("removing space in string :", string)
+
+# # r strip
+# string = "   sreedhar   deva   "
+# a = string.rstrip()
+# print("string")
+# # print("sreedhar", a, "deva")
+#
+# string = "   sreedhar   "
+# string = string.rstrip()
+# print("string:", string)
+#
+# string = "  @@@sreedhar***  "
+# string = string.lstrip()
+# print("string:", string)
+#
+#
+# string = "@@@  sreedhar   ***"
+# string = string.lstrip('@')
+# print("string:", string)
+#
+# string = "@@@sreedhar***"
+# string = string.lstrip('*')
+# print("string:", string)
+#
+# string = "@@@sreedhar***"
+# string = string.rstrip('@')
+# print("string:", string)
+#
+#
+# string = "@@@sreedhar***"
+# string = string.rstrip('*')
+# print("string:", string)
+#
+# string = "sreedhar"
+# string = string.replace("s", "S")
+# print("updated string:", string)
+#
+# string = "sreedhar"
+# string = string.replace("sreedhar", "s")
+# print("updated string:", string)
+#
+# string = "sreedhar@@daffsgff@@afdffd"
+# string = string.split('a')
+# print(string)
+#
+# string = "sreedhar@@daffsgff@@aaafdffd"
+# string = string.split('a')
+# print(string)
+#
+# string = ("sreedhar", "deva", "arun")
+# string ="bharath".join(string)
+# print(string)
+
+
+# string
+
